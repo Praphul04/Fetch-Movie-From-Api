5464e9fa43a9d7975f0fabc441137e98169db443
Praphul04 <praphulthakur6@gmail.com>
2023-08-22 16:56:32 +0530

    Adding new Movies intio the list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useCallback, useMemo } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
+
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const [retryCount, setRetryCount] = useState(0);
-  const [retryIntervalId, setRetryIntervalId] = useState(null);
 
   const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
@@ -29,57 +29,42 @@ function App() {
         };
       });
       setMovies(transformedMovies);
-      setRetryCount(0);
     } catch (error) {
       setError(error.message);
-      setRetryCount((prevRetryCount) => prevRetryCount + 1);
     }
     setIsLoading(false);
   }, []);
-
-  const cancelRetryHandler = useCallback(() => {
-    clearInterval(retryIntervalId);
-    setRetryCount(0);
-  }, [retryIntervalId]);
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  useEffect(() => {
-    if (retryCount > 0) {
-      setRetryIntervalId(
-        setInterval(() => {
-          fetchMoviesHandler();
-        }, 5000)
-      );
-    }
+  function addMovieHandler(movie) {
+    console.log(movie);
+  }
 
-    return () => {
-      clearInterval(retryIntervalId);
-    };
-  }, [retryCount, fetchMoviesHandler]);
+  let content = <p>Found no movies.</p>;
 
-  const content = useMemo(() => {
-    if (movies.length > 0) {
-      return <MoviesList movies={movies} />;
-    } else if (error) {
-      return (
-        <React.Fragment>
-          <p>{error}</p>
-          <button onClick={fetchMoviesHandler}>Retry</button>
-          <button onClick={cancelRetryHandler}>Cancel</button>
-        </React.Fragment>
-      );
-    } else if (isLoading) {
-      return <p>Loading...</p>;
-    } else {
-      return <p>Found no movies.</p>;
-    }
-  }, [movies, error, isLoading, fetchMoviesHandler, cancelRetryHandler]);
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
       <section>{content}</section>
     </React.Fragment>
   );
